8ed90dc0db0f4025e8d679dc239de1f68b5a01a7
TARANGTECH\harik <hari.kumar@mcpayment.net>
2017-10-27 15:50:20 +0530

    regression suite added

--- src/main/java/com/snyder/ui/object/AosMenuItems.java
+++ src/main/java/com/snyder/ui/object/AosMenuItems.java
@@ -9,6 +9,13 @@ public interface AosMenuItems {
 	By tv_appliances=By.xpath("//android.widget.TextView[@text='TVs & Appliances']");
 	By fashion=By.xpath("//android.widget.TextView[@text='Fashion']");
 	By home_furniture=By.xpath("//android.widget.TextView[@text='Home and Furniture']");
-	
+	By books_more=By.xpath("//android.widget.TextView[@text='Books & More']");
+	By offerzone=By.xpath("//android.widget.TextView[@text='Offer Zone']");
+	By notifications=By.xpath("//android.widget.TextView[@text='Notifications']");
+	By myrewards=By.xpath("//android.widget.TextView[@text='My Rewards']");
+	By myCart=By.xpath("//android.widget.TextView[@text='My Cart']");
+	By myWishList=By.xpath("//android.widget.TextView[@text='My Wishlist']");
+	By myOrders=By.xpath("//android.widget.TextView[@text='My Orders']");
+	By giftCard=By.xpath("//android.widget.TextView[@text='Gift Card']");
 
 }
