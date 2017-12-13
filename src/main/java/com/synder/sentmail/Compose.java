ffc25306ba196db6eebd1d86233ecd655eaeaf45
kumarkhk <khk.kumar19@gmail.com>
2017-12-13 21:33:18 +0530

    sent mail and compose are added

---
+++ src/main/java/com/synder/sentmail/Compose.java
@@ -0,0 +1,10 @@
+package com.synder.sentmail;
+
+public class Compose {
+
+	public static void main(String[] args) {
+		System.out.println("abc");
+
+	}
+
+}
---
+++ src/main/java/com/synder/sentmail/SentMail.java
@@ -0,0 +1,11 @@
+package com.synder.sentmail;
+
+public class SentMail {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Send mail method");
+		System.out.println("new ");
+	}
+
+}
