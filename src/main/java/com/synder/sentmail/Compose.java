5c585cfb52fed04a6efe0190e09398bc9be46f92
kumarkhk <khk.kumar19@gmail.com>
2017-12-13 21:41:49 +0530

    new chnages are updated

--- src/main/java/com/synder/sentmail/Compose.java
+++ src/main/java/com/synder/sentmail/Compose.java
@@ -3,8 +3,9 @@ package com.synder.sentmail;
 public class Compose {
 
 	public static void main(String[] args) {
-		System.out.println("abc");
+		
 		System.out.println("8yryg");
+		System.out.println("dhgfh");
 
 	}
 
--- src/main/java/com/synder/sentmail/SentMail.java
+++ src/main/java/com/synder/sentmail/SentMail.java
@@ -4,9 +4,10 @@ public class SentMail {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Send mail method");
+		
 		System.out.println("new ");
 		System.out.println("sdf");
+		System.out.println("7fdfsa");
 	}
 
 }
